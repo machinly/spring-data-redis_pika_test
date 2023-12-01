52f13167e0ea36cddc97edccf70ef146afc70f1b
machinly <machinlyg@gmail.com>
2023-12-01 20:11:40 +0800

    fix: use RESP2

--- src/main/java/com/machinly/spring/pika/springpika/RedisController.java
+++ src/main/java/com/machinly/spring/pika/springpika/RedisController.java
@@ -1,29 +1,40 @@
 package com.machinly.spring.pika.springpika;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.protocol.ProtocolVersion;
+import io.lettuce.core.protocol.RedisCommand;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class RedisController {
 
-    private final RedisExample redisExample;
+    private final RedisClient redis;
 
-    @Autowired
-    public RedisController(RedisExample redisExample) {
-        this.redisExample = redisExample;
+    public RedisController() {
+        this.redis = RedisClient.create("redis://:123456@127.0.0.1:9221");
+        ClientOptions clientOptions = ClientOptions.builder().
+                protocolVersion(ProtocolVersion.RESP2).
+                build();
+        this.redis.setOptions(clientOptions);
     }
 
     @PutMapping("/redis/set/{key}/{value}")
     public String setKeyValue(@PathVariable String key, @PathVariable String value) {
-        redisExample.setValue(key, value);
+        redis.connect().sync().set(key, value);
         return "Value set successfully";
     }
 
     @GetMapping("/redis/get/{key}")
     public String getKeyValue(@PathVariable String key) {
-        return redisExample.getValue(key);
+        return redis.connect().sync().get(key);
     }
 }
